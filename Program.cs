686c4ff3db6ce7066127289888e5485282c66eda
Issam Benelgada <issambenelgada@gmail.com>
2024-11-25 22:14:21 +0100

    Printing version.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace MyApp
 {
@@ -6,7 +7,8 @@ namespace MyApp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Version? version = Assembly.GetExecutingAssembly().GetName().Version;
+      Console.WriteLine($"Hello World! {version}");
     }
   }
 }
