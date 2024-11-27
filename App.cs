95304a65be5e17f17e3cd8167f075a18313ef7d5
Issam Benelgada <issambenelgada@gmail.com>
2024-11-28 00:02:30 +0100

    Refactored main class.

---
+++ App.cs
@@ -0,0 +1,49 @@
+using Serilog;
+
+namespace MagpyServerLinux;
+
+public class App
+{
+  public static async Task Start()
+  {
+    UpdateManager.Init();
+    Log.Debug("Updating setup finished.");
+
+    UpdateManager.SetupPeriodicUpdate();
+
+    AppDomain.CurrentDomain.ProcessExit += Program_Exited;
+    SignalWatcher.SetupSignalWatchers();
+
+    Log.Debug("Looking for node executable.");
+
+    bool nodefound = NodeManager.VerifyNodeExe();
+
+    if (!nodefound)
+    {
+      throw new Exception("Node executable not found.");
+    }
+
+    Log.Debug("Node executable found.");
+    Log.Debug("Staring node server.");
+
+    NodeManager.StartNodeServer();
+
+    Log.Debug("Node server started.");
+
+    await Task.Delay(500);
+
+    ServerManager.OpenWebInterface();
+
+    while (true)
+    {
+      await Task.Delay(100000);
+    }
+  }
+
+  private static void Program_Exited(object? sender, EventArgs e)
+  {
+    Log.Debug("Program closing, Killing node server.");
+    NodeManager.KillNodeServer();
+    InstanceManager.ReleaseInstance();
+  }
+}
--- Program.cs
+++ Program.cs
@@ -4,48 +4,11 @@ namespace MagpyServerLinux
 {
     public class Program
     {
-        static async Task StartApp()
-        {
-            UpdateManager.Init();
-
-            Log.Debug("Updating setup finished.");
-
-            UpdateManager.SetupPeriodicUpdate();
-
-            Log.Debug("Looking for node executable.");
-
-            bool nodefound = NodeManager.VerifyNodeExe();
-
-            if (!nodefound)
-            {
-                throw new Exception("Node executable not found.");
-            }
-
-            Log.Debug("Node executable found.");
-            Log.Debug("Staring node server.");
-
-            NodeManager.StartNodeServer();
-
-            Log.Debug("Node server started.");
-
-            await Task.Delay(500);
-
-            ServerManager.OpenWebInterface();
-
-            while (true)
-            {
-                await Task.Delay(100000);
-            }
-        }
-
         static async Task Main(string[] args)
         {
 #if DEBUG
             LoggingManager.InitEarly();
 #endif
-            AppDomain.CurrentDomain.ProcessExit += Program_Exited;
-            SignalWatcher.SetupSignalWatchers();
-
             try
             {
                 bool instanceCreated = InstanceManager.HoldInstance();
@@ -54,7 +17,7 @@ namespace MagpyServerLinux
                     LoggingManager.Init();
                     Log.Debug("Logging initialized.");
 
-                    await StartApp();
+                    await App.Start();
                 }
                 else
                 {
@@ -81,13 +44,6 @@ namespace MagpyServerLinux
             {
                 InstanceManager.ReleaseInstance();
             }
-        }
-
-        private static void Program_Exited(object? sender, EventArgs e)
-        {
-            Log.Debug("Program closing, Killing node server.");
-            NodeManager.KillNodeServer();
-            InstanceManager.ReleaseInstance();
         }
     }
 }
