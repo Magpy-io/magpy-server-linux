5179e595f3a7ed5183d3fc77092fc2cfc06c3524
Issam Benelgada <issambenelgada@gmail.com>
2024-11-28 00:02:56 +0100

    Refactor app class.

--- App.cs
+++ App.cs
@@ -1,49 +1,52 @@
 using Serilog;
 
-namespace MagpyServerLinux;
-
-public class App
+namespace MagpyServerLinux
 {
-  public static async Task Start()
+  public class App
   {
-    UpdateManager.Init();
-    Log.Debug("Updating setup finished.");
-
-    UpdateManager.SetupPeriodicUpdate();
-
-    AppDomain.CurrentDomain.ProcessExit += Program_Exited;
-    SignalWatcher.SetupSignalWatchers();
-
-    Log.Debug("Looking for node executable.");
-
-    bool nodefound = NodeManager.VerifyNodeExe();
-
-    if (!nodefound)
+    public static async Task Start()
     {
-      throw new Exception("Node executable not found.");
+      UpdateManager.Init();
+      Log.Debug("Updating setup finished.");
+
+      UpdateManager.SetupPeriodicUpdate();
+
+      AppDomain.CurrentDomain.ProcessExit += Program_Exited;
+      SignalWatcher.SetupSignalWatchers();
+
+      Log.Debug("Looking for node executable.");
+
+      bool nodefound = NodeManager.VerifyNodeExe();
+
+      if (!nodefound)
+      {
+        throw new Exception("Node executable not found.");
+      }
+
+      Log.Debug("Node executable found.");
+      Log.Debug("Staring node server.");
+
+      NodeManager.StartNodeServer();
+
+      Log.Debug("Node server started.");
+
+      await Task.Delay(500);
+
+      ServerManager.OpenWebInterface();
+
+      while (true)
+      {
+        await Task.Delay(100000);
+      }
     }
 
-    Log.Debug("Node executable found.");
-    Log.Debug("Staring node server.");
-
-    NodeManager.StartNodeServer();
-
-    Log.Debug("Node server started.");
-
-    await Task.Delay(500);
-
-    ServerManager.OpenWebInterface();
-
-    while (true)
+    private static void Program_Exited(object? sender, EventArgs e)
     {
-      await Task.Delay(100000);
+      Log.Debug("Program closing, Killing node server.");
+      NodeManager.KillNodeServer();
+      InstanceManager.ReleaseInstance();
     }
   }
 
-  private static void Program_Exited(object? sender, EventArgs e)
-  {
-    Log.Debug("Program closing, Killing node server.");
-    NodeManager.KillNodeServer();
-    InstanceManager.ReleaseInstance();
-  }
 }
+
