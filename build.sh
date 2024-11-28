580635e2660e3fe57dc118b6de275be2e64f502a
Issam Benelgada <issambenelgada@gmail.com>
2024-11-28 01:50:05 +0100

    Added build step to build.sh script.

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-vpk pack -u Magpy -v 1.0.0 -p ./bin/Release/net8.0/ --packTitle Magpy --packAuthors "Magpy Team." -e MagpyServerLinux
+dotnet build -c Release
+vpk pack -u Magpy -v 1.0.0 -p ./bin/Release/net8.0/ --packTitle Magpy --packAuthors "Magpy Team." -e Magpy
