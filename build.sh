8ce05207a6f72a2e3962d2ca5e9adb14efa90539
Issam Benelgada <issambenelgada@gmail.com>
2024-11-28 12:12:54 +0100

    Added publish self-contained to build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-dotnet build -c Release
-vpk pack -u Magpy -v 1.0.0 -p ./bin/Release/net8.0/ --packTitle Magpy --packAuthors "Magpy Team." -e Magpy
+# dotnet build -c Release
+dotnet publish --runtime linux-x64 --self-contained
+vpk pack -u Magpy -v 1.0.0 -p ./bin/Release/net8.0/linux-x64/publish --packTitle Magpy --packAuthors "Magpy Team." -e Magpy
